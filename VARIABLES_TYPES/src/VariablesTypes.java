c85a9317d0aada2de656c2d61bad4d8b4e9afa66
Davi Varella <davivarelladev@gmail.com>
2025-02-19 17:40:16 -0300

    operators and variables

---
+++ VARIABLES_TYPES/src/VariablesTypes.java
@@ -0,0 +1,8 @@
+public class VariablesTypes {
+    public static void main(String[] args) throws Exception {
+        String nomeUm = "Davi";
+        String nomeDois = new String("davi");
+
+        System.out.println(nomeDois.equals(nomeUm));
+    }
+}
